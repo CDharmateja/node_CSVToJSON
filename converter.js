1e3715b78f73c00fa107bee393b2cb35c54e9046
CDharmateja <cdharmateja1999@gmail.com>
2018-03-11 10:41:16 +0530

    converted to json

--- converter.js
+++ converter.js
@@ -1,5 +1,60 @@
 const fs = require('fs')
 const path = require('path')
 
-fs.readfile(path.join(__dirname, './customer-data.csv'), {encoding: UTF-8}, function() {
-}
+let json = [];
+
+fs.readFile(path.join(__dirname, './customer-data.csv'), {encoding: 'utf-8'}, function(error, data) {
+if (error) return  console.error(error);
+	const params = {};
+	let string = '';
+	let list = [];
+	for (i of data) {
+		if (i === '\r') {
+			continue;
+		}
+		if (i === '\n') {
+			params[string] = '';
+			list.push(string);
+			string = '';
+			break;
+		}
+		else if (i === ',') {
+			params[string] = '';
+			list.push(string);
+			string = '';
+		}
+		else {
+			string += i;
+		}
+	}
+	let max = list.length;
+	let temp = Object.assign({}, params);
+	let index = 0;
+	for (i of data) {
+		if (i === '\r') {
+			continue;
+		}
+		if (i === '\n') {
+			temp[list[index]] = string;
+			string = '';
+			json.push(temp);
+			temp = Object.assign({}, params);
+			index = 0;
+		}
+		else {
+			if (i === ',') {
+				temp[list[index]] = string;
+				string = '';
+				index++;
+			}
+			else {
+				string += i;
+			}
+		}
+	}
+	json.splice(0, 1);
+	fs.writeFile('customer-data.json', JSON.stringify(json), function(error) {
+		if (error) return console.erro(error);
+		console.log('writing is done');
+	});
+});
